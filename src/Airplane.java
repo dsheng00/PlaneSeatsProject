5017196e6637522f4d60797b8448d6684a076d2c
Zeliboid8 <zeliboid8@gmail.com>
2016-12-08 09:37:16 -0500

    Made all seats assigned to 0
    Added final int variables to change plane size

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,16 +1,39 @@
 public class Airplane 
 {
-	private int[][] rows = new int[5][];
+	public final int NUMBER_OF_FIRST_CLASS_ROWS = 5;
+	public final int NUMBER_OF_FIRST_CLASS_AISLES = 4;
+	public final int NUMBER_OF_ECONOMY_CLASS_ROWS = 15;
+	public final int NUMBER_OF_ECONOMY_CLASS_AISLES = 6;
+	private int[][] firstClassSeats = new int[NUMBER_OF_FIRST_CLASS_ROWS][];
+	private int[][] economyClassSeats = new int[NUMBER_OF_ECONOMY_CLASS_ROWS][];
 	
 	public Airplane()
 	{
-		for (int rowIndex = 0; rowIndex < 5; rowIndex++) // Assigns all the seats to 0 (Unoccupied)
+		for (int rowIndex = 0; rowIndex < NUMBER_OF_FIRST_CLASS_ROWS; rowIndex++)
 		{
-			rows[rowIndex] = new int[4];
-			for (int aisleIndex = 0; aisleIndex < 4; aisleIndex++)
+			firstClassSeats[rowIndex] = new int[NUMBER_OF_FIRST_CLASS_AISLES];
+			for (int aisleIndex = 0; aisleIndex < NUMBER_OF_FIRST_CLASS_AISLES; aisleIndex++)
 			{
-				rows[aisleIndex][rowIndex] = 0;
+				firstClassSeats[aisleIndex][rowIndex] = 0; // Assigns all the first class seats to 0 (Unoccupied)
 			}
 		}
+		for (int rowIndex = 0; rowIndex < NUMBER_OF_ECONOMY_CLASS_ROWS; rowIndex++)
+		{
+			economyClassSeats[rowIndex] = new int [NUMBER_OF_ECONOMY_CLASS_AISLES];
+			for (int aisleIndex = 0; aisleIndex < NUMBER_OF_ECONOMY_CLASS_AISLES; aisleIndex++)
+			{
+				economyClassSeats[aisleIndex][rowIndex] = 0; // Assigns all the economy class seats to 0
+			}
+		}
+	}
+	
+	public int getFirstClassSeatValue(int x, int y) // Checks if seat is occupied or unoccupied
+	{
+		return firstClassSeats[x][y];
+	}
+	
+	public int getEconomyClassSeatValue(int x, int y)
+	{
+		return economyClassSeats[x][y];
 	}
 }
