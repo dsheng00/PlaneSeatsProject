756e8bff940a7d5464b3116dcfab038291790510
David Sheng <shengji250@gmail.com>
2016-12-08 09:41:11 -0500

    More stuff added to SeatingCreator
    More things added to SeatingCreator

--- src/SeatingCreator.java
+++ src/SeatingCreator.java
@@ -4,13 +4,17 @@ public class SeatingCreator
 {
 	public static void main(String args[])
 	{
-		//some display method displaying the airplane layout
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Please enter the number of seats "
-							+ "you would like to reserve: ");
-		int num = sc.nextInt();
-		//method that takes the number of people
-		System.out.print("Do you have any seating preference? ");
-		
+		while(SENTINEL != ) //sentinel
+			//some display method displaying the airplane layout
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Please enter the number of seats "
+								+ "you would like to reserve: ");
+			int num = sc.nextInt();
+			//method that takes the number of people
+			System.out.print("Do you have any seating preference? "
+								+ "Window, aisle, or middle?");
+			//method that makes preferences
+			//method(s) assigns seats
+			
 	}
 }
