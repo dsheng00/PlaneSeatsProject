2216d25680be544affdaf92795cb5605b2f8da4c
David Sheng <shengji250@gmail.com>
2016-12-09 23:05:43 -0500

    Alphabet loop in Display class
    Previous issue fixed

--- src/Display.java
+++ src/Display.java
@@ -3,33 +3,27 @@ public class Display
 	public static void printSeating(Airplane airplane)
 	{
 		char c = 'A';
-		while (c <= 'F') 
+		for (int i = 0; i < airplane.NUMBER_OF_FIRST_CLASS_ROWS; i++)
 		{
-			for (int i = 0; i < airplane.NUMBER_OF_FIRST_CLASS_ROWS; i++)
-			{	
-				System.out.print(c + " ");
-				for (int j = 0; j < airplane.NUMBER_OF_FIRST_CLASS_AISLES; j++)
-				{
-					System.out.print(intToLetter(airplane.getSeatValue("first", i, j)) + " ");
-				}
-				c++;
-				System.out.println();
+			System.out.print(c + " ");
+			for (int j = 0; j < airplane.NUMBER_OF_FIRST_CLASS_AISLES; j++)
+			{
+				System.out.print(intToLetter(airplane.getSeatValue("first", i, j)) + " ");
 			}
+			System.out.println();
+			c++;
 		}
 		System.out.println();
-		
-		while(c <= 'U')
-		{		
-			for (int i = 0; i < airplane.NUMBER_OF_ECONOMY_CLASS_ROWS; i++)
+
+		for (int i = 0; i < airplane.NUMBER_OF_ECONOMY_CLASS_ROWS; i++)
+		{
+			System.out.print(c + " ");
+			for (int j = 0; j < airplane.NUMBER_OF_ECONOMY_CLASS_AISLES; j++)
 			{
-				System.out.print(c + " ");
-				for (int j = 0; j < airplane.NUMBER_OF_ECONOMY_CLASS_AISLES; j++)
-				{
-					System.out.print(intToLetter(airplane.getSeatValue("economy", i, j)) + " ");
-				}
-				System.out.println();
-				c++;
+				System.out.print(intToLetter(airplane.getSeatValue("economy", i, j)) + " ");
 			}
+			System.out.println();
+			c++;
 		}
 	}
 	
