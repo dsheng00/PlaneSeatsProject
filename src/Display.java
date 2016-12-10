f8b74e3928c9e96daaaa024d9646af8671428ab8
Zeliboid8 <zeliboid8@gmail.com>
2016-12-09 21:40:39 -0500

    Added code to Display class
    Now prints out the seating arrangement (lines aren't centered yet
    though)
    Temporarily changed SeatingArrayTester to test the Display class.

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,33 @@
 public class Display 
 {
+	public static void printSeating(Airplane airplane)
+	{
+		for (int i = 0; i < airplane.NUMBER_OF_FIRST_CLASS_ROWS; i++)
+		{
+			for (int j = 0; j < airplane.NUMBER_OF_FIRST_CLASS_AISLES; j++)
+			{
+				System.out.print(intToLetter(airplane.getSeatValue("first", i, j)) + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 
+		for (int i = 0; i < airplane.NUMBER_OF_ECONOMY_CLASS_ROWS; i++)
+		{
+			for (int j = 0; j < airplane.NUMBER_OF_ECONOMY_CLASS_AISLES; j++)
+			{
+				System.out.print(intToLetter(airplane.getSeatValue("economy", i, j)) + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static String intToLetter(int x)
+	{
+		if (x == 1)
+		{
+			return "O"; // "O" for occupied
+		}
+		return "V"; // "V" for vacant
+	}
 }
--- src/SeatingArrayTester.java
+++ src/SeatingArrayTester.java
@@ -2,9 +2,17 @@ public class SeatingArrayTester
 {
 	public static void main(String[] args)
 	{
+		Airplane boeing = new Airplane();
+		Display.printSeating(boeing);
+		
+		
+		
+		
+		/* For later use
 		Airplane boeing1337 = new Airplane();
 		System.out.println(boeing1337.getSeatValue("first", 2, 3));
 		boeing1337.occupySeat("first", 2, 3);
 		System.out.println(boeing1337.getSeatValue("first", 2, 3));
+		*/
 	}
 }
