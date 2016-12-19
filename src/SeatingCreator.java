f1f11515215cdf53b29dc78dcfed2a2a3b395bc1
Zeliboid8 <zeliboid8@gmail.com>
2016-12-19 09:41:42 -0500

    Changed seatingCreator
    Added extra questions.

--- src/SeatingCreator.java
+++ src/SeatingCreator.java
@@ -21,37 +21,42 @@ public class SeatingCreator
 		{	
 			Display.printSeating(plane);
 			System.out.println();
-			System.out.print("Reserve or remove seat ticket(s)?");
+			System.out.print("Would you like to choose or automatically assign seats? ");
 			String reChoice = sc.nextLine();
-			if (reChoice.equalsIgnoreCase("reserve"))
+			if (reChoice.equalsIgnoreCase("choose"))
 			{
-				System.out.print("Please enter the name(s) you would like to "
-						+ "reserve the seat(s) under: ");
+				System.out.print("Please enter the seat name(s) you would like to reserve: ");
 				String names = sc.nextLine(); 
 				namesToArray(names);
 				
 				for(String element : namesList)
 				{
 					System.out.print("First class or economy for " + element + "? "); 
-					String flightClass = sc.nextLine();
-					System.out.print("Do you have any seating preference? "
-							+ "Window, aisle, or middle? ");
 					String pref = sc.nextLine();
 					//TODO method that makes preferences using contain() and finds the right seat
 					//TODO method assigns seats
 					//TODO correspond each seat with a name
 				}
 			}
+			
+			else if (reChoice.equalsIgnoreCase("automatically assign seats"))
+			{
+				String flightClass = sc.nextLine();
+				System.out.println("Do you have any seating preference? "
+					+ "Window, aisle, or middle? ");
+				
+			}
 			else
 			{
-				//a search system of some sort
-				//and/or remove a given seat number
+				System.out.println("That is not a valid option.");
 			}
+			
 			System.out.print("Continue? Y/N: ");
 			response = sc.nextLine();
 			System.out.println();
 		}
 		sc.close();	
+		Display.printSeating(plane);
 	}
 	
 	private static void namesToArray(String names)
