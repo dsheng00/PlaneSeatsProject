fdb54782a5ce587db8e581b2e8c1ba1f991fcab5
Zeliboid8 <zeliboid8@gmail.com>
2016-12-20 09:59:40 -0500

    Removed unnecessary methods, changed array type
    Array type changed to an array of an array of Seat objects.
    Got rid of occupySeat and removeSeat.

--- src/Airplane.java
+++ src/Airplane.java
@@ -4,63 +4,27 @@ public class Airplane
 	public final int NUMBER_OF_FIRST_CLASS_AISLES = 4;
 	public final int NUMBER_OF_ECONOMY_CLASS_ROWS = 15;
 	public final int NUMBER_OF_ECONOMY_CLASS_AISLES = 6;
-	private int[][] firstClassSeats;
-	private int[][] economyClassSeats;
+	private Seat[][] firstClassSeats;
+	private Seat[][] economyClassSeats;
 	
 	public Airplane()
 	{
-		firstClassSeats = new int[NUMBER_OF_FIRST_CLASS_ROWS][];
-		economyClassSeats = new int[NUMBER_OF_ECONOMY_CLASS_ROWS][];
+		firstClassSeats = new Seat[NUMBER_OF_FIRST_CLASS_ROWS][];
+		economyClassSeats = new Seat[NUMBER_OF_ECONOMY_CLASS_ROWS][];
 		for (int rowIndex = 0; rowIndex < NUMBER_OF_FIRST_CLASS_ROWS; rowIndex++)
 		{
-			firstClassSeats[rowIndex] = new int[NUMBER_OF_FIRST_CLASS_AISLES];
+			firstClassSeats[rowIndex] = new Seat[NUMBER_OF_FIRST_CLASS_AISLES];
 			for (int aisleIndex = 0; aisleIndex < NUMBER_OF_FIRST_CLASS_AISLES; aisleIndex++)
 			{
-				firstClassSeats[rowIndex][aisleIndex] = 0; // Assigns all the first class seats to 0 (Unoccupied)
+				firstClassSeats[rowIndex][aisleIndex].occupy(); // Assigns all the first class seats to unoccupied
 			}
 		}
 		for (int rowIndex = 0; rowIndex < NUMBER_OF_ECONOMY_CLASS_ROWS; rowIndex++)
 		{
-			economyClassSeats[rowIndex] = new int [NUMBER_OF_ECONOMY_CLASS_AISLES];
+			economyClassSeats[rowIndex] = new Seat[NUMBER_OF_ECONOMY_CLASS_AISLES];
 			for (int aisleIndex = 0; aisleIndex < NUMBER_OF_ECONOMY_CLASS_AISLES; aisleIndex++)
 			{
-				economyClassSeats[rowIndex][aisleIndex] = 0; // Assigns all the economy class seats to 0
+				economyClassSeats[rowIndex][aisleIndex].occupy(); // Assigns all the economy class seats to unoccupied
 			}
 		}
 	}
-	
-	public int getSeatValue(String seatingClass, int x, int y) // Checks if seat is occupied or unoccupied
-	{
-		if (seatingClass.equalsIgnoreCase("first"))
-		{
-			return firstClassSeats[x][y];
-		}
-		return economyClassSeats[x][y];
-	}
-	
-	public void occupySeat(String seatingClass, int x, int y) // Occupies the seat in the given seat coordinate
-	{ //add name later?
-		if (seatingClass.substring(0, 5).equalsIgnoreCase("first") 
-				|| seatingClass.equalsIgnoreCase("1st")) 
-		{
-			firstClassSeats[x - 1][y - 1] = 1; // Since array indices start at 0, 1 is subtracted
-		}
-		else
-		{
-			economyClassSeats[x - 1][y - 1] = 1;
-		}
-	}
-	
-	public void removeSeat(String seatingClass, int x, int y) //might have to use name too
-	{
-		if (seatingClass.substring(0, 5).equalsIgnoreCase("first") 
-				|| seatingClass.equalsIgnoreCase("1st")) 
-		{
-			firstClassSeats[x - 1][y - 1] = 0; // Since array indices start at 0, 1 is subtracted
-		}
-		else
-		{
-			economyClassSeats[x - 1][y - 1] = 0;
-		}
-	}
-}
